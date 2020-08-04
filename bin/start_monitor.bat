2584f3826959450099a8027887fe2ef173f4caa3
mplesser <mlesser@arizona.edu>
2020-08-04 09:02:14 -0700

    cleanup

--- bin/start_monitor.bat
+++ bin/start_monitor.bat
@@ -1,3 +1,3 @@
 @echo off
 
-python C:\azcam\azcammonitor\azcammonitor.py -configfile "/azcam/azcam-mont4k/bin/parameters_mont4k_monitor.ini"
+start/min python C:\azcam\azcammonitor\azcammonitor.py -configfile "/azcam/azcam-mont4k/bin/parameters_mont4k_monitor.ini"
