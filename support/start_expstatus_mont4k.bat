a099cfdb68e015bd53c15ecab455bace7b7228ae
Michael Lesser <mlesser@arizona.edu>
2023-09-01 16:45:25 -0700

    cleanup

---
+++ support/start_expstatus_mont4k.bat
@@ -0,0 +1,5 @@
+rem Examples for starting expstatus
+
+rem PowerShell -WindowStyle Hidden "python -m azcam_expstatus.expstatus - -port 2402"
+
+rem C:\Python311\pythonw.exe -m azcam_expstatus.expstatus -port 2402
