6a3873e2c6b42c25a0ee94ca2cda9b38108c17ea
Michael Lesser <mlesser@arizona.edu>
2024-02-06 14:17:35 -0700

    update

--- support/datafolder/bin/start_monitor_mont4k.bat
+++ support/datafolder/bin/start_monitor_mont4k.bat
@@ -1,5 +1,7 @@
 @echo off
 
-start/min "azcammonitor" python -m azcam_monitor.azcammonitor -- -configfile "/data/mont4k/parameters/parameters_monitor_mont4k.ini"
+rem start/min "azcammonitor" python -m azcam_monitor.azcammonitor -- -configfile "/data/mont4k/parameters/parameters_monitor_mont4k.ini"
 
-start "azcammonitor" ipython -m azcam_monitor.azcammonitor -i -- -configfile "/azcam/azcam-mont4k/support/datafolder/parameters/parameters_monitor_mont4k.ini"
+rem start "azcammonitor" ipython -m azcam_monitor.azcammonitor -i -- -configfile "/azcam/azcam-mont4k/support/datafolder/parameters/parameters_monitor_mont4k.ini"
+
+ipython -m azcam_monitor.azcammonitor -i -- -configfile "/azcam/azcam-mont4k/support/datafolder/parameters/parameters_monitor_mont4k.ini"
